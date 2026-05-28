fe5aba0efd04da42a77bfb5a23fa706bb7dec041
Ritika Rathore <ritikarathorerr16@gmail.com>
2026-05-29 01:46:47 +0530

    Day 2: Added user input and investment calculator

---
+++ src/basic/task.py
@@ -0,0 +1,47 @@
+# # DAY1 TASK
+# print("Ritika rathore")
+
+# portfolio_value = 100000
+
+# print(portfolio_value)
+# import pandas as pd
+# crypto_data={
+#     "Crypto":["Bitcoin", "Ethereum","Tether"]
+# }
+# df= pd.DataFrame(crypto_data)
+# print(df)
+
+
+#DAY2
+# investment= float(input("Enter your investment amount:"))
+# profit= investment*0.12
+# print("Expected profit:",profit)
+
+# task1
+# User se:
+# favorite stock name
+# stock price
+# input lo.
+
+
+# import pandas as pd
+# stock_name= input("Enter your favorite stock name: ")
+# stock_price= float(input("Enter stock price: "))
+
+# stock_data={
+#     "stock": [stock_name],
+#     "price": [stock_price]
+# }
+# df = pd.DataFrame(stock_data)
+# print(df)
+
+# Task 2
+# User se:
+# investment amount
+# lo and:
+# 15% return calculate karo
+
+investment = float(input("enter investment amount : "))
+stock_return = investment * 0.15
+print("return :",stock_return)
+print("Total Value :", investment + stock_return)
